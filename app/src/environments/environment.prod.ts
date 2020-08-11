1559062e4eebb2e3a217c196c3604e2a4e34f873
dtadessse87@gmail.com <davemtadesse@gmail.com>
2020-08-10 23:28:13 -0500

    post instal

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-API_URL:'http://localhost:3000' || 'https://debt-free-calculator.herokuapp.com/',
+  API_URL:'http://localhost:3000' || 'https://debt-free-calculator.herokuapp.com/',
  };
